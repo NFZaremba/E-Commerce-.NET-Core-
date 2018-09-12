195a7d30438566832cea5635db3581efdec794dd
Nick <nfz32@comcast.net>
2018-09-12 14:53:13 -0700

    first comment

---
+++ SquashBox/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SquashBox.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
